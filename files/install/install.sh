a55eb22afe0bef993113bb291180a5ea37cbb8a0
C4Wiz <c4wiz@live.com>
2017-01-01 17:36:06 -0700

    Update install.sh

--- files/install/install.sh
+++ files/install/install.sh
@@ -335,7 +335,7 @@ rm -Rf /var/lib/tomcat7/webapps/ROOT
 ln -s /var/lib/tomcat7/webapps/guacamole.war /var/lib/tomcat7/webapps/ROOT.war 
 
 ### Compensate for GUAC-513
-ln -s /usr/local/lib/freerdp/guacsnd.so /usr/lib/x86_64-linux-gnu/freerdp/ 
+ln -s /usr/local/lib/freerdp/guacsnd.so /usr/lib/x86_64-linux-gnu/freerdp/
 ln -s /usr/local/lib/freerdp/guacdr.so /usr/lib/x86_64-linux-gnu/freerdp/
 
 # openbox confg

a88e20db464aa55f12fb4156d52650cfae92a71d
2devnull <2devnull@users.noreply.github.com>
2017-01-18 15:53:14 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -p 8080:8080 -p 3389:3389 -e "TZ=America/Denver" --name DockerGui dockergui
+docker run -d -p 3389:3389 -e "TZ=America/New_York" --name DockerGui dockergui
 
